870608b8ba8e4e5a0964681596a637279075e4c2
Erezana Berisha <erzanaberisha2001@gmail.com>
2026-06-23 02:36:56 +0200

    Display saved tasks

---
+++ src/ui/TaskBox.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { Task } from '../types/Task';
+
+type TextBoxProps = {
+  task: Task;
+};
+
+export default function TextBox({ task }: TextBoxProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        {task.title}
+      </Text>
+
+      <Text>
+        {task.description}
+      </Text>
+
+      <Text style={styles.date}>
+        {task.createdDate}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 16,
+    marginTop: 12,
+    padding: 16,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 6,
+  },
+  date: {
+    marginTop: 8,
+    fontSize: 12,
+    color: '#777',
+  },
+});
