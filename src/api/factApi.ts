724c5153bc1be8dde44558bac3de9f5453357c68
Erezana Berisha <erzanaberisha2001@gmail.com>
2026-06-23 10:22:43 +0200

    Add random fact API integration

---
+++ src/api/factApi.ts
@@ -0,0 +1,8 @@
+export const fetchFact = async () => {
+  const response = await fetch(
+    'https://uselessfacts.jsph.pl/api/v2/facts/random?language=en'
+  );
+  
+  const data = await response.json();
+  return data;
+};
--- src/screens/TaskList/index.tsx
+++ src/screens/TaskList/index.tsx
@@ -6,18 +6,50 @@ import { RootStackParamList } from '../../navigation/types';
 import useTasks from '../../store/useTasks';
 import Button from '../../ui/Button';
 import TaskBox from '../../ui/TaskBox';
+import { useEffect, useState } from 'react';
+import { fetchFact } from '../../api/factApi';
+import { Ionicons } from '@expo/vector-icons';
 
 type TaskListNavigation = NativeStackNavigationProp<
   RootStackParamList,
   'TaskList'
 >;
 
+
 export default function TaskList() {
+  const [fact, setFact] = useState('');
   const navigation = useNavigation<TaskListNavigation>();
   const tasks = useTasks((state) => state.tasks);
 
+  useEffect(() => {
+  const loadFact = async () => {
+    try {
+      const data = await fetchFact();
+      setFact(data.text);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  loadFact();
+}, []);
   return (
     <View>
+      <View style={styles.factBox}>
+        <View style={styles.factHeader}>
+           <Ionicons
+          name="earth"
+          size={24}
+            color="green"
+          />
+            <Text style={styles.factTitle}>
+            Did You Know?
+            </Text>
+        </View>
+        <Text>
+          {fact}
+        </Text>
+      </View>
       {tasks.length === 0 ? (
         <Text style={styles.noTaskText}>
           No tasks yet.
@@ -43,6 +75,24 @@ export default function TaskList() {
 const styles = StyleSheet.create({
   noTaskText: {
     marginHorizontal: 10,
-    paddingVertical: 14,
+    padding: 16,
   },
+  factBox: {
+  margin: 16,
+  padding: 16,
+  backgroundColor: '#F5F5F5',
+},
+
+factHeader: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginBottom: 12,
+},
+
+factTitle: {
+  fontSize: 16,
+  fontWeight: '600',
+  marginLeft: 8,
+  color: '#222',
+},
 });
